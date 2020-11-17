681be55bf242f660e49f9437da8193e85b1846c3
Yifan Ai <ayfallen@hotmail.com>
2020-11-17 22:23:01 +1100

    Implement updateTimeslot by id

--- timeslotAPI/timeslotAPI/v1/api/timeslots_id.py
+++ timeslotAPI/timeslotAPI/v1/api/timeslots_id.py
@@ -12,16 +12,45 @@ from .. import schemas
 class TimeslotsId(Resource):
 
     @staticmethod
-    def get(id):
-        requestedID = id
+    def get_all_timeslots():
         with open('timeslots.json', 'r') as jsonFile:
             timeslots = json.loads(jsonFile.read())
+        return timeslots
+
+    @staticmethod
+    def update_all_timeslots(timeslots):
+        with open('timeslots.json', 'w') as jsonFile:
+            json.dump(timeslots, jsonFile)
+        return
+
+    def get_timeslot_by_id(self, requestedID):
+        timeslots = self.get_all_timeslots()
         for timeslot in timeslots:
             if str(timeslot['id']) == requestedID:
-                return timeslot, 200, None
+                return timeslot
+        return None
+
+    def patch_timeslot_by_id(self, requestedID, desiredTimeslot):
+        timeslots = self.get_all_timeslots()
+        hasMatchedTimeslot = False
+        for timeslot in timeslots:
+            if str(timeslot['id']) == requestedID:
+                hasMatchedTimeslot = True
+                timeslot['status'] = desiredTimeslot['status']
+                break
+        if hasMatchedTimeslot:
+            self.update_all_timeslots(timeslots)
+            return desiredTimeslot
+        return None
+
+    def get(self, id):
+        timeslot = self.get_timeslot_by_id(id)
+        if timeslot:
+            return timeslot, 200, None
         return {}, 404, None
 
     def patch(self, id):
-        print(g.json)
-
-        return {'date': 'something', 'dentist': 'something', 'startTime': 'something', 'status': 'something'}, 200, None
+        updatedTimeslot = self.patch_timeslot_by_id(id, g.json)
+        if updatedTimeslot:
+            return updatedTimeslot, 200, None
+        return {}, 404, None
