7251e0343745c4c5a5f5348522dd5698d025fc68
Yifan Ai <ayfallen@hotmail.com>
2020-11-17 19:59:54 +1100

    Implement listTimeslots including query

--- timeslotAPI/timeslotAPI/v1/api/timeslots.py
+++ timeslotAPI/timeslotAPI/v1/api/timeslots.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function
 
+import json
+
 from flask import request, g
 
 from . import Resource
@@ -9,7 +11,18 @@ from .. import schemas
 
 class Timeslots(Resource):
 
-    def get(self):
-        print(g.args)
-
-        return [], 200, None
+    @staticmethod
+    def get():
+        POSSIBLE_PARAMETERS = ['dentist', 'date', 'startTime', 'status']
+        with open('timeslots.json', 'r') as jsonFile:
+            timeslots = json.loads(jsonFile.read())
+        matchedTimeslots = []
+        for timeslot in timeslots:
+            isMatched = True
+            for parameter in POSSIBLE_PARAMETERS:
+                if g.args.get(parameter) and timeslot[parameter] != g.args.get(parameter):
+                    isMatched = False
+                    break
+            if isMatched:
+                matchedTimeslots.append(timeslot)
+        return matchedTimeslots, 200, None
